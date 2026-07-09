6d6417bc0dde6e3980c3d3ccff313191dab1786a
titus0405 <ckbatam0405@gmail.com>
2026-07-09 17:11:16 +0700

    new1

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-ns-coffee-conversion-key-change-in-production')
 DEBUG = os.environ.get('DEBUG', 'True').lower() in ('true', '1', 'yes')
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,0.0.0.0').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,0.0.0.0,.vercel.app').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
